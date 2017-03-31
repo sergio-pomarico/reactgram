baa0d3a14f9c913c0cdaa10838bc43be38959b78
Sergio Pomárico <sergiodavid21@gmail.com>
2017-03-31 02:43:24 -0500

    add firebase to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import firebase from 'firebase';
+
+let config = {
+    apiKey: "AIzaSyBhWO3XTGYUbvVe1dKSPFBbVxgxpLVrkmI",
+    authDomain: "reactgram-3eee0.firebaseapp.com",
+    databaseURL: "https://reactgram-3eee0.firebaseio.com",
+    projectId: "reactgram-3eee0",
+    storageBucket: "reactgram-3eee0.appspot.com",
+    messagingSenderId: "911138207745"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
