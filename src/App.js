5e08ab7fe03f1c18308a2c9e95000802af186889
Sergio Pomárico <sergiodavid21@gmail.com>
2017-03-31 03:36:40 -0500

    login with google and firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+
+    this.state = {
+      'user': null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({user})
+    });
+  }
+
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+            .then(result => console.log(`${result.user.email} is logged in`))
+            .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout(){
+    firebase.auth().signOut()
+            .then(result => console.log(`${result.user.email} is logged out`))
+            .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton(){
+    if(this.state.user){
+      return (
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} width="250"/>
+          <p>Hello {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Logout</button>
+        </div>
+      );
+    }else{
+      return(
+        <button onClick={this.handleAuth}>Login with Google</button>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Reactgram</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.renderLoginButton()}
         </p>
       </div>
     );
